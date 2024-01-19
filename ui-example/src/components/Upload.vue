513ebbb672beb53959f17284c535fad47dfba05e
gappc <gapp.christian@gmail.com>
2024-01-19 13:08:34 +0100

    Improve upload UI

--- ui-example/src/components/Upload.vue
+++ ui-example/src/components/Upload.vue
@@ -2,9 +2,11 @@
   <div>
     <div>
       Upload file:
-      <input ref="fileInput" type="file" @change="handleFileUpload()" />
+      <input ref="fileInput" type="file" @change="handleFileInputChange()" />
+      <button @click="handleFileUpload">Upload</button>
     </div>
-    <pre>{{ uploadMessage }}</pre>
+    <pre v-if="uploading">Uploading...</pre>
+    <pre style="color: green">{{ uploadMessage }}</pre>
     <pre style="color: red">{{ errorMessage }}</pre>
   </div>
 </template>
@@ -17,6 +19,12 @@ const fileInput = ref<HTMLInputElement | null>(null);
 
 const uploadMessage = ref("");
 const errorMessage = ref("");
+const uploading = ref(false);
+
+const handleFileInputChange = () => {
+  uploadMessage.value = "";
+  errorMessage.value = "";
+};
 
 const handleFileUpload = async () => {
   console.log("handleFileUpload", fileInput.value?.files);
@@ -32,28 +40,35 @@ const handleFileUpload = async () => {
 const uploadFiles = async (files: File[]) => {
   uploadMessage.value = "";
   errorMessage.value = "";
+  uploading.value = true;
 
   const formData = new FormData();
   formData.append("file", files[0], files[0].name);
   // files.forEach((file) => formData.append(file.name, file));
 
-  fetch("/api/upsert-file", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (response.status >= 400) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((result) => {
-      console.log("Success:", result);
-      uploadMessage.value = JSON.stringify(result);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      uploadMessage.value = JSON.stringify(error);
+  try {
+    const response = await fetch("/api/upsert-file", {
+      method: "POST",
+      body: formData,
     });
+
+    if (response.status >= 400) {
+      console.log("Error:", response);
+      const message = {
+        status: response.status,
+        statusText: response.statusText,
+        body: await response.text(),
+      };
+      throw new Error(JSON.stringify(message));
+    }
+
+    uploadMessage.value = await response.text();
+  } catch (error) {
+    console.error("Error:", error);
+    errorMessage.value =
+      error instanceof Error ? error.message : JSON.stringify(error);
+  } finally {
+    uploading.value = false;
+  }
 };
 </script>
