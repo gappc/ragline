c0629fa2fbf806d69134e4ad30ea84e8f2e361e9
gappc <gapp.christian@gmail.com>
2024-01-19 12:37:24 +0100

    Show documents to index in log

--- better_example/server/utils.py
+++ better_example/server/utils.py
@@ -9,4 +9,7 @@ def do_query(query):
 
 def do_upsert_file(path: str):
     documents = SimpleDirectoryReader(input_files=[path]).load_data()
+    print("----------------Documents to index----------------")
+    print([x for x in documents])
+    print("----------------End documents to index----------------")
     return index_documents(documents)
