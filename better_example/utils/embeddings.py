00321220c2c344db12812223e69ebf81c33c0f38
gappc <gapp.christian@gmail.com>
2024-01-24 01:38:36 +0100

    Set embedding batch size
    Higher batch sizes needs more RAM.

--- better_example/utils/embeddings.py
+++ better_example/utils/embeddings.py
@@ -1,5 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from llama_index.embeddings import BaseEmbedding, HuggingFaceEmbedding
 
+load_dotenv()
+
+embed_batch_size = os.getenv("EMBEDDING_BATCH_SIZE", 1)
+
 embedding_model: BaseEmbedding = HuggingFaceEmbedding(
-    model_name="BAAI/bge-small-en-v1.5", cache_folder="./.cache/huggingface"
+    model_name="BAAI/bge-small-en-v1.5",
+    cache_folder="./.cache/huggingface",
+    embed_batch_size=embed_batch_size,
 )
